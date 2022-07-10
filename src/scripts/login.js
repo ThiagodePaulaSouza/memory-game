e7a29e8db817d97b47be2aba34c14f6a09271a0a
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-10 14:38:02 -0300

    feat: see system's color scheme and do the switch theme

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,12 +1,39 @@
 const input = document.querySelector(".login-input");
 const button = document.querySelector(".login-button");
-const form = document.querySelector('.login-form');
-const checkbox = document.querySelector('#checkbox')
+const form = document.querySelector(".login-form");
+const checkbox = document.querySelector("#checkbox");
+const body = document.querySelector("body");
+const dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-// switch toggle dark/light theme
-checkbox.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+// switch system toggle dark/light theme
+if (dark) {
+  checkbox.checked = true;
+  body.classList.add("dark");
+  input.classList.add("dark");
+}
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", function (e) {
+    const colorScheme = e.matches ? "dark" : "light";
+    console.log(colorScheme);
+
+    if (colorScheme === "dark") {
+      checkbox.checked = true;
+      body.classList.add("dark");
+      input.classList.add("dark");
+    } else {
+      checkbox.checked = false;
+      body.classList.remove("dark");
+      input.classList.remove("dark");
+    }
+  });
+
+// switch button toggle dark/light theme
+checkbox.addEventListener("change", () => {
+  body.classList.toggle("dark");
+  input.classList.toggle("dark");
+});
 
 const validarInput = ({ target }) => {
   if (target.value.length > 2) {
@@ -20,9 +47,9 @@ const validarInput = ({ target }) => {
 input.addEventListener("input", validarInput);
 
 const handleSubmit = (event) => {
-    event.preventDefault()
-    localStorage.setItem('player', input.value)
-    window.location = './src/pages/game.html'
-}
+  event.preventDefault();
+  localStorage.setItem("player", input.value);
+  window.location = "./src/pages/game.html";
+};
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
