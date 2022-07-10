60258deaf071d2c08285c1ab7b24e598b85e7dc1
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-10 05:05:09 -0300

    add: toggle switch theme
    change light theme to sypia like to don't hurt our eyes & setted light theme as default
    
    remove: header.css

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,6 +1,12 @@
 const input = document.querySelector(".login-input");
 const button = document.querySelector(".login-button");
 const form = document.querySelector('.login-form');
+const checkbox = document.querySelector('#checkbox')
+
+// switch toggle dark/light theme
+checkbox.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
 
 const validarInput = ({ target }) => {
   if (target.value.length > 2) {
